8285da0a124119fce25b7a01904068ba8be1764f
riri3 <kamijooor@gmail.com>
2021-09-27 20:41:15 +0900

    chap14 update

--- Ex1_13_2.java
+++ Ex1_13_2.java
@@ -13,26 +13,34 @@
 public class Ex1_13_2 {
 	public static void main (String[] args) {
 
+		//	定数
+		final String MESSAGE_A		=	"あなたは几帳面な性格の方ですね？";
+		final String MESSAGE_B		=	"あなたはマイペースな性格の方ですね？";
+		final String MESSAGE_O		=	"あなたは大雑把な性格ですね？";
+		final String MESSAGE_AB		=	"あなたは天才肌な方ですね？";
+		final String MESSAGE_DEFAULT=	"数字の1～4で答えてください";
+
+
 		int bloodType = Integer.parseInt(args[0]);
 		switch(bloodType){
 			case 1:
-				System.out.println("あなたは几帳面な性格の方ですね？");
+				System.out.println(MESSAGE_A);
 				break;
 
 			case 2:
-				System.out.println("あなたはマイペースな性格の方ですね？");
+				System.out.println(MESSAGE_B);
 				break;
 
 			case 3:
-				System.out.println("あなたは大雑把な性格ですね？");
+				System.out.println(MESSAGE_O);
 				break;
 
 			case 4:
-				System.out.println("あなたは天才肌な方ですね？");
+				System.out.println(MESSAGE_AB);
 				break;
 
 			default:
-				System.out.println("数字の1～4で答えてください");
+				System.out.println(MESSAGE_DEFAULT);
 				break;
 		}
 	}
