167df812551fd077f4b84d993351813d7f940d2e
riri3 <kamijooor@gmail.com>
2021-09-30 01:26:04 +0900

    chap15-2

--- Ex1_14_2.java
+++ Ex1_14_2.java
@@ -1,52 +1,33 @@
-/*-< 演習：Ex1_14_2 >---------------------------------
-コマンドライン引数から1つ以上の数値を受け取り、この1つ1つの数値を購入した商品の定価としたとき、
-合計金額を求め、その金額に応じた値引きを利かせ、最後に消費税（8％とする）をのせた金額を表示するプログラムを作りたい。
-mainメソッドの内容は書き換えずに処理が通るようなメソッドを仕様に従って書いてください。
-----------------------------------------------------*/
-public class Ex1_14_2 {
-	
-	//！！！mainメソッドの処理は書き換えないでください！！！
+class Ex1_14_2{
 	public static void main (String[] args) {
-		
-		int totalPrice = 0 ;                                 //購入した商品の合計金額
-		
-		//コマンドライン引数から購入した商品の合計金額（定価）を取得
-		for(int i = 0 ; i < args.length ; i++ ){
-			totalPrice += Integer.parseInt( args[i] );
+
+		int coinNum = args.length - 1;							//投入された硬貨の枚数
+		int price = Integer.parseInt(args[args.length - 1]);	//購入された商品の値段
+		int coinChk = 0 ;                                	    //チェック対象の硬貨の金額
+		int coinSum = 0 ;                                       //投入された硬貨の合計金額 ※使用可能な硬貨のみで算出
+		int change = 0;                                     	//お釣り
+
+		for ( int i=0 ; i < coinNum ; i++ ) {
+			coinChk = Integer.parseInt(args[i]) ;
+
+			if ( coinChk == 10 || coinChk == 50 || coinChk == 100 || coinChk == 500 ) {
+				System.out.println(coinChk + "円が投入されました");
+				coinSum += coinChk;
+			}else if( coinChk == 1 || coinChk == 5 ) {
+				System.out.println("警告：" + coinChk + "円玉は使えません");
+			}else {
+				System.out.println("警告：" + coinChk + "は硬貨として適切な値ではありません");
+			}
 		}
-		
-		//discountメソッドを使って値引きを適用し、値引き後の金額を取得
-		int discountedPrice = discount( totalPrice );        //値引き後の金額
-		
-		//calcTaxPaymentメソッドを使って支払金額（税込）を取得
-		int taxPayment = calcTaxPayment( discountedPrice );  //支払金額（税込）
-		
-		//支払金額（税込）を表示
-		System.out.println("値引き後の支払金額：" + taxPayment + "円" );
-		
-	}
-	
-	
-	/*
-	**以下の仕様を持つメソッドcalcTaxPaymentを作成してください。
-	**  - 引数として受け取った値の税込価格（消費税は8％とする）を計算して返す
-	**  - 税込価格は整数（小数点以下切り捨て）で返す
-	*/
-	static ______ calcTaxPayment( __________________ ){        //アンダーバーを適切な内容に書き換えてください
-		
-		
-	}
-	
-	
-	/*
-	**以下の仕様を持つメソッドdiscountを作成してください。
-	**  - 引数として受け取った値が1000円以上5000円以下の場合、1000円より大きい分について10％割引にする
-	**  - 引数として受け取った値が5000円より大きい場合、1000円より大きく5000円以下の分について10％割引、5000円より大きい分について20％割引にする
-	**  - 値引き額は小数点以下切り捨てで計算する（キャストを使いましょう）
-	**  - 割引金額の上限は5000円とする
-	*/
-	static ______ discount( __________________ ){            //アンダーバーを適切な内容に書き換えてください
-		
-		
+		//合計金額から購入された商品の値段を引いてお釣りを算出
+		change = coinSum - price ;
+
+		if (change > 0) {
+			System.out.println("お釣りは" + change + "円です。ありがとうございました。");
+		}else if( change == 0 ){
+			System.out.println("ちょうどのお預かりです。ありがとうございました。");
+		}else {
+		System.out.println("お金が" + Math.abs(change) + "円足りません。追加で投入してください。");
+		}
 	}
 }
