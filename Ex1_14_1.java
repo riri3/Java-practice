9a19f307b1bb5a3feb62b1b7677a721585d68a25
riri3 <kamijooor@gmail.com>
2021-09-30 00:08:52 +0900

    chap15-1 update

--- Ex1_14_1.java
+++ Ex1_14_1.java
@@ -10,19 +10,20 @@ class Ex1_14_1 {
 			//このループでチェックする硬貨をセット
 			coinChk = Integer.parseInt(args[i]);
 
-		if ( coinChk == 10 || coinChk == 50 || coinChk == 100 || coinChk == 500 ) {
-			System.out.println("１円玉は使えません");
+			if ( coinChk == 10 || coinChk == 50 || coinChk == 100 || coinChk == 500 ) {
+				coinSum += coinChk;
+				System.out.println("ただいまの投入金額は" + coinSum +"円です");
+			}
 
-		}
-		else if ( coinChk == 5 ) {
-			System.out.println("５円玉は使えません");
+			else if ( coinChk == 1 || coinChk == 5) {
+				System.out.println( "警告：" + coinChk + "円玉は使えません");
+			}
+
+			else {
+				System.out.println( "警告：" + coinChk + "は硬貨として適切な値ではありません" );
+			}
 		}
 
-		else {
-			System.out.println( coinChk + "は硬貨として適切な値ではありません" );
-		}
 		System.out.println("ただいまの投入金額は" + coins +"円です");
-		}
-
 	}
 }
